1fc58df2fd68070d89f84acf5e86944a0b235e58
Yahya Sopian <yahyasopian5@gmail.com>
2024-11-24 16:25:39 +0700

    mengubah variabel menjadi bentuk objek dan menyelesaikan latihan 1.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -29,7 +31,7 @@ export default App;
 // Header component
 const Header = (props) => {
   console.log(props);
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 // Part component for Content component
